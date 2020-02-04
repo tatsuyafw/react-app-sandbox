faf6729cc365e05ea807b21e02a7b8ede0453577
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2020-02-05 08:47:19 +0900

    Introduce useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  });
 
   return (
     <div className="App">
